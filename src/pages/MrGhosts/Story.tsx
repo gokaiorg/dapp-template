f5fbd32f1e672992f3cda076a84a1f0c90e81271
Jeremy <jeremydouchamps@gmail.com>
2022-03-13 12:39:04 +0700

    Update mr ghost story

--- src/pages/MrGhosts/Story.tsx
+++ src/pages/MrGhosts/Story.tsx
@@ -6,14 +6,19 @@ const Story = () => {
   return (
     <div className="mr-ghosts-story bg-mr-ghost-bottom d-flex flex-column text-white w-100 py-5 h2 text-center">
       <p className="mt-5 mb-3 mx-auto col-md-6">
-        666 traits will build all the Maiar Ghosts.
+        Maiar Ghosts is a 8658 unique Mr Ghost NFT collection on the Elrond
+        blockchain that was born from a wish to create an NFT around the M logo
+        from the Elrond Network ​​digital wallet: Maiar.
       </p>
       <p className="mb-3 mx-auto col-md-6">
-        A total of 13 drops of 666 NFTs will be generated for a maximum total of
-        8658 NFTs.
+        Maiar Ghosts are made up of 9 attributes including 265 expressions, 75
+        backgrounds, 99 decorations, 8 bodies, 78 tools, 61 hats, 39 clouds, 23
+        jewels and 18 transports. For a total of 666 character traits.
       </p>
       <p className="mb-3 mx-auto col-md-6">
-        Each owner will be a member of Gokai DAO.
+        Some of the layers will be named for popular Elrond Network projects of
+        our time. It will be like a snapshot of the birth of NFT projects on
+        Elrond and Maiar wallet.
       </p>
       <motion.div
         className="mrghost-animated col-md-4"
