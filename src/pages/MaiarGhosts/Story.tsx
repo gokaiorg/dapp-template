a691651c7b99acaed9cb4d631db820b160ee9f53
Jeremy <jeremydouchamps@gmail.com>
2022-04-04 13:06:38 +0700

    Update maiar ghosts story

--- src/pages/MaiarGhosts/Story.tsx
+++ src/pages/MaiarGhosts/Story.tsx
@@ -5,21 +5,23 @@ import MrGhostDriver from "../../assets/img/mr-ghost-driving-elrond-nft-dao-maia
 const Story = () => {
   return (
     <div className="mr-ghosts-story bg-mr-ghost-bottom d-flex flex-column text-white w-100 py-5 h2 text-center">
-      <p className="mt-5 mb-3 mx-auto col-md-6">
+      <p className="mt-5 mb-3 mx-auto col-md-6 mb-5">
         Maiar Ghosts is a 8658 unique Mr Ghost NFT collection on the Elrond
         blockchain that was born from a wish to create an NFT around the M logo
         from the Elrond Network ​​digital wallet: Maiar.
       </p>
-      <p className="mb-3 mx-auto col-md-6">
-        Maiar Ghosts are made up of 9 attributes including 265 expressions, 75
-        backgrounds, 99 decorations, 8 bodies, 78 tools, 61 hats, 39 clouds, 23
-        jewels and 18 transports. For a total of 666 character traits.
-      </p>
-      <p className="mb-3 mx-auto col-md-6">
-        Some of the layers will be named for popular Elrond Network projects of
-        our time. It will be like a snapshot of the birth of NFT projects on
-        Elrond and Maiar wallet.
-      </p>
+      <motion.div whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }}>
+        <a
+          href="https://medium.com/@GokaiLabs/gokai-labs-mr-ghost-elrond-nft-around-the-maiar-logo-44bf2099a1ee"
+          data-testid="Medium Story - Mr Ghost NFT - Maiar Ghosts collection - Gokai Labs"
+          title="Medium Story - Mr Ghost NFT - Maiar Ghosts collection - Gokai Labs"
+          target="_blank"
+          rel="noreferrer"
+          className="btn btn-link btn-lg mx-auto d-inline-flex mt-5"
+        >
+          Read More
+        </a>
+      </motion.div>
       <motion.div
         className="mr-ghost-animated col-md-4"
         animate={{
